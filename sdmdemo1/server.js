a129fd8736448f389633974d3d9db31d43a1d169
kavita <sehgalkavita27@gmail.com>
2023-01-01 16:00:55 +0530

    done3

--- sdmdemo1/server.js
+++ sdmdemo1/server.js
@@ -2,9 +2,11 @@ var express= require("express");
 const app=express();
 var bodyparse=require("body-parser");
 var path=require("path");
-app.get("/",function(req,resp){
-    resp.sendFile(path.join(__dirname,"./public/index.html",));
-});
+app.use(express.static(path.join(__dirname,"public")));
+
+// app.get("/",function(req,resp){
+//     resp.sendFile(path.join(__dirname,"./public/index.html",));
+// });
 
 app.listen(5000);
 console.log("server is listening on port 5000");
